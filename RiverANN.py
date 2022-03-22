c504862131f89440625fa374f91a2cd122394ac7
Mutsa01 <80479759+Mutsa01@users.noreply.github.com>
2022-03-22 21:31:28 +0000

    implement forward probigation on sample data

--- RiverANN.py
+++ RiverANN.py
@@ -1,4 +1,5 @@
 from itertools import count
+from math import exp
 from random import random
 from random import seed
 
@@ -15,12 +16,47 @@ def dataset_minmax(dataset):
 	minmax = list()
 	stats = [[min(column), max(column)] for column in zip(*dataset)]
 	return stats
+
+def deNormalizeOutput(value, min, max):
+    return((value-0.1)*(max-min)/0.8)+min
  
-# Rescale dataset columns to the range 0-1
+# Rescale dataset columns to the range 0.1-0.9
 def normalize_dataset(dataset, minmax):
 	for row in dataset:
 		for i in range(len(row)-1):
 			row[i] = 0.8*(row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])+0.1
+
+
+
+def activate(weights, inputs):
+	activation = weights[-1]
+	for i in range(len(weights)-1):
+		activation += weights[i] * inputs[i]
+	return activation
+
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+    inputs = row 
+    for layer in network:
+        new_inputs = []
+        count = 0
+        for neuron in layer:
+
+            activation = activate(neuron['weights'], inputs)
+            print("activation: ",activation)
+
+            if count == 0:
+                neuron['output'] = transfer(activation)
+            else:
+                neuron['output'] = activation
+            new_inputs.append(neuron['output'])
+            #count+=1
+        inputs = new_inputs
+        #print("layer",layer)
+    return inputs
 
 #network is a list of arrays (layers) and neurons are dictionaries
 def initialize_network(n_inputs, n_hidden, n_outputs):
@@ -39,12 +75,24 @@ def showNetwork(NN):
 
 
 
-
-nInputs = len(data[0])
+#-1 because last input = expected value
+nInputs = len(data[0])-1
 nOutputs = 1
 network = initialize_network(nInputs, 2, nOutputs)
+
+expectedArr=[]
+for i in range (0,len(data)):
+    expectedArr.append(data[i][-1])
+
+expMin= min(expectedArr)
+expMax = max(expectedArr)
+
 #showNetwork(network)
 minmax = dataset_minmax(data)
 normalize_dataset(data, minmax)
-print(data)
+#print(data)
+for row in data:
+    #print(row[:-1])
+    output = forward_propagate(network, row[:-1])
+    print(deNormalizeOutput(output[0], expMin,expMax))
 
